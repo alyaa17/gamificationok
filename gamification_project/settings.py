d64f13c33bfa139adb26c826737d665f273581f7
Alina <alina17ru@outlook.com>
2023-03-06 00:44:08 +0300

    static.modified

--- gamification_project/settings.py
+++ gamification_project/settings.py
@@ -110,13 +110,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = 'static/'
 
 if DEBUG:
-        STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
-        ]
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
