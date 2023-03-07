380469c436b46b2c51ae0bbe1602594f9cf1138a
alina <alina17ru@icloud.com>
2023-03-07 12:03:21 +0300

    2

--- gamification_project/settings.py
+++ gamification_project/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = 'admin/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = '/home/voronezh/voronezh.pythonanywhere.com/static/staticr'
+STATIC_ROOT = '/home/voronezh/voronezh.pythonanywhere.com/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
