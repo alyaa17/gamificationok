b1403448430f8fcc45cce843911c6e2e04df3e24
alina <alina17ru@icloud.com>
2023-03-07 10:16:51 +0300

    e

--- gamification_project/settings.py
+++ gamification_project/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'staticr/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = 'Desktop\gamification\static\staticr'
+STATIC_ROOT = '/home/voronezh/voronezh.pythonanywhere.com/static/staticr'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
